f9927df2b8704e5eaac6ba29f206f96387583411
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-18 08:52:32 +0000

    Deploy rfrolov/otus_homework_06 to github.com/rfrolov/otus_homework_06.git:gh-pages

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['main',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
-  ['matrix',['matrix',['../classmatrix.html#a2e1c0cad73c8e5b6ca6c216fb1a27b22',1,'matrix']]]
+  ['matrix',['matrix',['../classmatrix.html#a2e1c0cad73c8e5b6ca6c216fb1a27b22',1,'matrix::matrix()'],['../classmatrix.html#a7f6df7b4f8a789c0ca3bfc05fa35e7aa',1,'matrix::matrix(const matrix &amp;other)'],['../classmatrix.html#a7d4bb27055785284fa9d03a537d147d6',1,'matrix::matrix(matrix &amp;&amp;other) noexcept']]]
 ];
